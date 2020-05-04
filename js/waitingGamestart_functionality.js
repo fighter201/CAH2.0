8e60d4975109c4b1e9fa3b36b738a92a80f78ee4
reicherBene <61832950+reicherBene@users.noreply.github.com>
2020-05-04 20:09:01 +0200

    Unterscheidung host/member (waitingGamestart)

--- js/waitingGamestart_functionality.js
+++ js/waitingGamestart_functionality.js
@@ -1,4 +1,5 @@
 console.log("waitingGamestart_functionality.js loaded");
+const head = document.getElementById('head');
 const spanInsideLobby = document.getElementById('insideLobby');
 const spanMaxLobby = document.getElementById('maxLobby');
 const ulPlayerList = document.getElementById('playerList');
@@ -39,6 +40,12 @@ function getHeight(element){
 function getCSSHeight(element){
 	return element.style.height;
 }
+function addStylesheet(sheetName){
+	var link = htmlElement('link');
+	link.rel = "stylesheet";
+	link.href = "../css/"+sheetName+".css";
+	head.appendChild(link);
+}
 //=================================================================
 
 function setInsideLobby(insideLobby){
@@ -74,21 +81,96 @@ function removePlayer(playerName){
 	console.log("removed "+playerName);
 }
 
-//					string		number 		number 		string[]
-function initLobby(lobbyName, insidePlayer, maxPlayer, players /*,settings...*/){
+function onSettingsChanged(input, oldValue){
+	if(input.value!=oldValue){
+		console.log("settingsChanged::buttonApplyActivated: input.value:"+input.value+" oldValue:"+oldValue);
+		btnStart.disabled = true;
+		btnChange.disabled = false;
+	}
+	else{
+		console.log("settingsChanged::buttonApplyDeactivated");
+		btnStart.disabled = false;
+		btnChange.disabled = true;
+	}
+}
+
+//					string		number 		number 		string[]  number	number
+function LobbyHost(lobbyName, insidePlayer, maxPlayer, players, maxPoints, numbCards /*,settings...*/){
+	//default setting values
+	this.maxPoints = 10;
+	this.numbCards = 10;
+	const timeLimit = "disabled";
+	
+	this.maxPoints = maxPoints;
+	this.numbCards = numbCards;
+	
+	//init Lobby
 	h2LobbyName.innerHTML = lobbyName;
 	setInsideLobby(insidePlayer);
 	setMaxLobby(maxPlayer);
 	
 	for (x in players) addPlayer(players[x]);
+	
+	//init host settings
+	const settingList = document.getElementById('settingListHost');
+	var li1 = htmlElement('li');
+	var span1 = htmlElement('span', "", "Round ends at ");
+	var span1EndingText = document.createTextNode(" points");
+	var input1 = htmlElement('input', 'maxPoints');
+	
+	var li2 = htmlElement('li');
+	var span2 = htmlElement('span', "", "White Cards per player: ");
+	var input2 = htmlElement('input', 'numbCards');
+	
+	var li3 = htmlElement('li');
+	var span3 = htmlElement('span', "", "Time limit: ");
+	var input3 = htmlElement('select', 'timeLimit');
+	var option31 = htmlElement('option', "", timeLimit);
+	
+	input1.setAttribute('type', "number");
+	input1.setAttribute('value', maxPoints);
+	input1.setAttribute('min', "1");
+	input1.addEventListener('change', function(){
+		onSettingsChanged(input1, maxPoints);
+	});
+	
+	input2.setAttribute('type', "number");
+	input2.setAttribute('value', numbCards);
+	input2.setAttribute('min', "1");
+	input2.addEventListener('change', function(){
+		onSettingsChanged(input2, numbCards);
+	});
+	
+	input3.addEventListener('change', function(){
+		onSettingsChanged(input3, timeLimit);
+	});
+	option31.selected = true;
+	
+	span1.appendChild(input1);
+	span1.appendChild(span1EndingText);
+	li1.appendChild(span1);
+	settingList.appendChild(li1);
+	
+	span2.appendChild(input2);
+	li2.appendChild(span2);
+	settingList.appendChild(li2);
+	
+	input3.appendChild(option31);
+	span3.appendChild(input3);
+	li3.appendChild(span3);
+	settingList.appendChild(li3);
+	
 }
-
-btnChange.addEventListener('click', function(){
+function initLobbyMember(lobbyName, insidePlayer, maxPlayer, players, maxPoints, numbCards /*,settings...*/){
+	h2LobbyName.innerHTML = lobbyName;
+	setInsideLobby(insidePlayer);
+	setMaxLobby(maxPlayer);
+	//settings
+	setMaxPoints(maxPoints);
+	setNumbCards(numbCards);
 	
-});
-btnStart.addEventListener('click', function(){
-	
-});
+	for (x in players) addPlayer(players[x]);
+}
 
 
 //mainMinSize(numberToCheckTo, numberToAdd) -> Ändere nur, wenn kleiner als... 
@@ -138,10 +220,37 @@ function startGame() {
     socket.emit('gameStart', gameID);
         }
 */
-		
-		
+
+//event Listener für settingInputs
+
+btnChange.addEventListener('click', function(){
+	
+});
+btnStart.addEventListener('click', function(){
+	
+});
+
 window.onload = function(){
 	console.log("onLoad()");
-	var players = ["ready", "totally ready", "hi"];
-	initLobby("Automatically generated Lobby", 3, 5, players);
+	
+	//vars werden von Server abgefragt
+	var isHost = true;
+	var lobbyName = "Automatically generated ";
+	var players = ["ready", "totally ready", "hi", "Der ist besonders lang"];
+	var playerIn = players.length;
+	var playerMx = 5;
+	var maxPnts = 10;
+	var numbCrds = 10;
+	//================================
+	
+	if(isHost){
+		addStylesheet('waitingGamestart_host');
+		var lobby1 = new LobbyHost(lobbyName+"Host Lobby", playerIn, playerMx, players, maxPnts, numbCrds);
+		btnStart.disabled = false;
+		btnChange.disabled = true;
+	}
+	else{
+		addStylesheet('waitingGamestart_member');
+		initLobbyMember(lobbyName+"Member Lobby", playerIn, playerMx, players, maxPnts, numbCrds);
+	}
 };
