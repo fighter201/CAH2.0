9f8545123e2ca37b17c0419165bcfcd9b87b62e7
reicherBene <61832950+reicherBene@users.noreply.github.com>
2020-05-05 00:03:05 +0200

    Update game.html, black- & whitecards

---
+++ js/game_functionality.js
@@ -0,0 +1,17 @@
+console.log("loaded game_functionality.js");
+
+/*
+var gameID = window.sessionStorage.getItem('gameID');
+    var playerID = 'defplayer';
+    // var playerID = window.sessionStorage.getItem('playerID');
+    socket.emit('inGame', gameID, playerID);
+    socket.on('roundInfo', function(czar, BC, WC, scoreboard) {
+        //display data
+        if (czar) {
+            //display Czar waiting screen
+        } else {
+            //display Slaves choose screen
+        }
+
+});
+*/
