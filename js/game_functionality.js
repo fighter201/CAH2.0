adf48c237f9388f1132e5f57df5d3c650a7b044c
reicherBene <61832950+reicherBene@users.noreply.github.com>
2020-05-05 18:01:05 +0200

    Update game.html, basic scoreboard

--- js/game_functionality.js
+++ js/game_functionality.js
@@ -1,4 +1,81 @@
 console.log("loaded game_functionality.js");
+const head = document.getElementById('head');
+const tableScoreboard = document.getElementById('scoreboard');
+
+//requires usefulFunctions.js 
+
+//scoreboard: rounds played/to go, ranking
+function ScoreBoardPlayer(name, score){
+	this.name = name;
+	this.id = "player_"+name.replace(/\s/g, "_");
+	this.score = score;
+	this.rank = 0;
+	this.html;
+	
+	var trPlayer = htmlElement('tr', this.id);
+	var tdName = htmlElement('td', "", this.name);
+	this.tdScore = htmlElement('td', "", this.score);
+	this.tdRank = htmlElement('td', "", "#"+this.rank);
+	
+	tdName.addClasses(['tableCell', 'nameCell']);
+	this.tdScore.addClasses(['tableCell','scoreCell']);
+	this.tdRank.addClasses(['tableCell', 'rankCell']);
+	
+	trPlayer.appendChild(tdName);
+	trPlayer.appendChild(this.tdScore);
+	trPlayer.appendChild(this.tdRank);
+	this.html = trPlayer;
+	
+	//Methoden
+	this.updateScore = function(score, rank){
+		this.score = score;
+		this.rank = rank;
+		this.tdScore.innerHTML = score;
+		this.tdRank.innerHTML = "#"+rank;
+	};
+	this.increaseScore = function(){
+		this.score++;
+	}
+	this.getScore = function(){ return this.score; }
+	this.getRank = function(){ return this.rank; }
+	
+}
+//					number		string[]
+function ScoreBoard(numbRounds, players){
+	this.numbRounds = numbRounds;
+	this.nowRound = 0;
+	this.scoreBoardPlayers = [];
+	
+	for(x in players){
+		var newPlayer = new ScoreBoardPlayer(players[x], 0);
+		this.scoreBoardPlayers.push(newPlayer);
+		tableScoreboard.appendChild(newPlayer.html);
+	}
+	
+	/*Methoden: countRound() -> Rundenzahl erhöhen; 
+				addPlayer(name) -> Spieler hinzufügen;
+				addPoint(playerName) -> Punkt an Spieler vergeben;
+	*/
+	this.countRound = function(){ this.nowRound++;}
+	this.addPlayer = function(name){
+		var newPlayer = new ScoreBoardPlayer(name, 0);
+		this.scoreBoardPlayers.push(newPlayer);
+		tableScoreboard.appendChild(newPlayer.html);
+	}
+	this.getPlayer = function(name){
+		for(x in this.scoreBoardPlayers){
+			if(name==this.scoreBoardPlayers[x].name) return this.scoreBoardPlayers[x];
+		}
+	}
+	this.addPoint = function(playerName){
+		var p = this.getPlayer(playerName);
+		p.increaseScore();
+		p.updateScore(p.getScore(), 0);
+	}
+	this.doRanking = function(){
+		
+	};
+}
 
 /*
 var gameID = window.sessionStorage.getItem('gameID');
@@ -15,3 +92,10 @@ var gameID = window.sessionStorage.getItem('gameID');
 
 });
 */
+
+window.onload = function(){
+	var scoreBoard = new ScoreBoard(10, ["Player1", "Player2", "dritterSpieler", "der schlechteste Spieler ever!!!"]);
+	scoreBoard.addPlayer("ich bin auch dabei");
+	scoreBoard.countRound();
+	scoreBoard.addPoint("Player1");
+};
---
+++ js/usefulFunctions.js
@@ -0,0 +1,43 @@
+const theHead = document.getElementById('head');
+
+//useful functions ================================================
+String.prototype.isEmpty = function(){
+	return (this.length === 0 || !this.trim());
+};
+function addClass(element, theClass){
+	if(!element.classList.contains(theClass)) element.classList.add(theClass);
+}
+function addClasses(element, classes){
+	for (x in classes){
+		if(!element.classList.contains(classes[x])) element.classList.add(classes[x]);
+	}
+}
+HTMLElement.prototype.addClass = function(theClass){
+	if(!this.classList.contains(theClass)) this.classList.add(theClass);
+};
+HTMLElement.prototype.addClasses = function(classes){
+	for (x in classes){
+		if(!this.classList.contains(classes[x])) this.classList.add(classes[x]);
+	}
+};
+function switchClass(element, adding, removing){
+	if(element.classList.contains(removing)) element.classList.remove(removing);
+	if(element.classList.contains(adding)) element.classList.remove(adding);
+	addClass(element, adding);
+}
+function removeClass(element, theClass){
+	if(element.classList.contains(theClass)) element.classList.remove(theClass);
+}
+function htmlElement(type, id, innerHTML){
+	var theElement = document.createElement(type);
+	if(typeof id !== "undefined") theElement.id = id;
+	if(typeof innerHTML !== "undefined") theElement.innerHTML = innerHTML;
+	return theElement;
+}
+function addStylesheet(sheetName){
+	var link = htmlElement('link');
+	link.rel = "stylesheet";
+	link.href = "../css/"+sheetName+".css";
+	theHead.appendChild(link);
+}
+//=================================================================
