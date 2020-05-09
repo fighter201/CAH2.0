0bb9b94fecb1d14b04cd2c383ca6b3d7d17e657f
reicherBene <61832950+reicherBene@users.noreply.github.com>
2020-05-09 22:36:29 +0200

    Update game_functionality.js

--- js/game_functionality.js
+++ js/game_functionality.js
@@ -1,14 +1,10 @@
 console.log("loaded game_functionality.js");
-const head = document.getElementById('head');
-const tableScoreboard = document.getElementById('scoreboard');
-const whiteCardContainer = document.getElementById('whiteCards');
-const chosseCardContainer = document.getElementById('chooseCards');
+
 var sb; //Scoreboard 
 var gc; //gameContainer
 
 //requires usefulFunctions.js 
 
-//scoreboard: rounds played/to go, ranking
 function ScoreBoardPlayer(name, score){
 	this.name = name;
 	this.id = "player_"+name.replace(/\s/g, "_");
@@ -49,27 +45,24 @@ function ScoreBoardPlayer(name, score){
 	this.getRank = function(){ return this.rank; }
 	
 }
-//					number		string[]
-function ScoreBoard(numbRounds, players){
-	this.numbRounds = numbRounds;
-	this.nowRound = 0;
+//					string[]
+function ScoreBoard(players){
 	this.scoreBoardPlayers = [];
 	
 	for(x in players){
 		var newPlayer = new ScoreBoardPlayer(players[x], 0);
 		this.scoreBoardPlayers.push(newPlayer);
-		tableScoreboard.appendChild(newPlayer.html);
+		gc.tableScoreboard.appendChild(newPlayer.html);
 	}
 	
 	/*Methoden: countRound() -> Rundenzahl erhöhen; 
 				addPlayer(name) -> Spieler hinzufügen;
 				addPoint(playerName) -> Punkt an Spieler vergeben;
 	*/
-	this.countRound = function(){ this.nowRound++;}
 	this.addPlayer = function(name){
 		var newPlayer = new ScoreBoardPlayer(name, 0);
 		this.scoreBoardPlayers.push(newPlayer);
-		tableScoreboard.appendChild(newPlayer.html);
+		gc.tableScoreboard.appendChild(newPlayer.html);
 		this.doRanking();
 	}
 	this.getPlayer = function(name){
@@ -88,9 +81,6 @@ function ScoreBoard(numbRounds, players){
 		//add: push->end; unshift->beginning
 		//remove: pop->end; shift->beginning
 		
-		//console.log("first time r[i]");
-		//for(i in r) console.log(r[i]);
-		
 		//sort players by points
 		for(x in p){
 			p[x].setRank(0);
@@ -104,22 +94,15 @@ function ScoreBoard(numbRounds, players){
 						placed = true;
 						break;
 					}
-					//console.log("wasn't bigger: "+x);
 				}
-				if(!placed){
-					r.push(x);
-					//console.log("pushed "+x);
-				}
+				if(!placed) r.push(x);
 			}
 		}
-		console.log("second time r[i]");
-		for(i in r) console.log(r[i]+" score:"+p[r[i]].score);
 		
 		//set Rank based on position in r
 		var beforeScore = 0;
 		var playersSameRank = 0;
 		for(i in r){
-			//console.log("doRanking::findRank	-> r["+i+"]:"+r[i]+" name:"+p[r[i]].name);
 			p[r[i]].html.removeClass('first');
 			if(i==0){
 				p[r[i]].setRank(1);
@@ -138,22 +121,35 @@ function ScoreBoard(numbRounds, players){
 			beforeScore = p[r[i]].score;
 		}
 		for(x in p) p[x].updateScoreBoard();
-		//console.log("third time r[i]");
-		//for(i in r) console.log(r[i]+" score:"+p[r[i]].score);
 	};
 }
+//Speicherort für alle relevanten Settings
 function GameContainer(){
 	this.content_blackScore = document.getElementById('content_blackScore');
+	this.blackCardCont = document.getElementById('blackCardCont');
+	this.scoreBoardCont = document.getElementById('scoreBoardCont');
 	this.divide_afterBlackScore = document.getElementById('divide_afterBlackScore');
 	this.content_czarWait = document.getElementById('content_czarWait');
 	this.content_whiteCards = document.getElementById('content_whiteCards');
 	this.content_chooseWinner = document.getElementById('content_chooseWinner');
 	this.content_endscreen = document.getElementById('content_endscreen');
-	//TODO: Auswahl Rundengewinner, Endscreen
 	
-	this.getHtml = function(elementName){
+	this.tableScoreboard = document.getElementById('scoreboard');
+	this.whiteCardContainer = document.getElementById('whiteCards');
+	this.chooseCardContainer = document.getElementById('chooseCards');
+	
+	this.blackCard = "default black Card";
+	this.answerNum = 0;
+	this.burnable = true;
+	this.whiteCards = [];
+	this.czar = false;
+	this.blankCardsAllowed = false;
+	
+	this.getHtmlOf = function(elementName){
 		switch(elementName){
 			case "content_blackScore": return this.content_blackScore;
+			case "blackCardCont": return this.blackCardCont;
+			case "scoreBoardCont": return this.scoreBoardCont;
 			case "divide_afterBlackScore": return this.divide_afterBlackScore;
 			case "content_czarWait": return this.content_czarWait;
 			case "content_whiteCards": return this.content_whiteCards;
@@ -163,69 +159,124 @@ function GameContainer(){
 		}
 	}
 	this.setVisible = function(elementName, status){
-		var e = this.getHtml(elementName);
+		var e = this.getHtmlOf(elementName);
 		if(status) e.switchClass('show', 'hide');
 		else{ e.switchClass('hide', 'show'); }
 		console.log("setVisible -> "+elementName+":"+status);
 	}
+	this.getWhiteCard = function(index){
+		if(index > this.whiteCards.length) return;
+		return this.whiteCards[index];
+	}
+	this.setBlackCard = function(text, answerNum){
+		this.blackCard = text;
+		this.answerNum = answerNum;
+		document.getElementById('blackCardText').innerHTML = text;
+		for(x in this.whiteCards) this.whiteCards[x].applyAnswerPossibilities();
+		//evtl: wenn Zeichenlänge überschritten wird, Schrift kleiner machen
+	}
+	this.setBurnable = function(burnable){
+		this.burnable = burnable;
+	}
 }
-function Card(text, hasAnswer, hasDoubleAnswer){
+function Card(text, index, burnable){
 	this.text = "Hallo ich bin der Inhalt einer weissen Karte. Und das sind meine Gedanken.";
 	this.burnable = true;  //based on game settings
-	this.hasAnswer = true;
-	this.hasDoubleAnswer = false; //based on game settings
+	this.index = -1;
+	this.answerNum = 0;
 	
 	this.text = text;
-	this.hasAnswer = hasAnswer;
-	this.hasDoubleAnswer = hasDoubleAnswer;
+	this.burnable = burnable;
+	this.index = index;
 	
 	//Generate HTMLElements
-	var divCard = htmlElement('div');
+	var divCard = htmlElement('div', "card_"+this.index);
 	var divClose = htmlElement('div');
-	this.spanClose = htmlElement('span');
+	this.spanClose = htmlElement('span', "card_"+this.index);
 	var divText = htmlElement('div');
 	var spanText = htmlElement('span', "", this.text);
-	var divBottom = htmlElement('div');
+	this.divBottom = htmlElement('div');
 	this.button1;
 	this.button2;
-	if(this.hasDoubleAnswer){
-		this.button1 = htmlElement('button', "", "Play as 1");
-		this.button2 = htmlElement('button', "", "Play as 2");
-	}
-	else{ this.button1 = htmlElement('button', "", "Play"); }
+	this.button3;
 	
 	divCard.addClass('card');
 	divClose.addClass('card_close');
 	if(this.burnable) divClose.addClass('show');
 	this.spanClose.addClass('close');
 	divText.addClass('card_text');
-	divBottom.addClass('card_bottom');
+	this.divBottom.addClass('card_bottom');
 	
 	divClose.appendChild(this.spanClose);
 	divText.appendChild(spanText);
-	divBottom.appendChild(this.button1);
-	if(this.hasDoubleAnswer) divBottom.appendChild(this.button2);
 	divCard.appendChild(divClose);
 	divCard.appendChild(divText);
-	divCard.appendChild(divBottom);
+	divCard.appendChild(this.divBottom);
 	this.html = divCard;
 	
-	this.button1.addEventListener('click', function(){
-		/*
-		var innerhtml = this.innerHTML;
-		var namePos = innerhtml.indexOf("lobbyName");
-		var nameStart = innerhtml.indexOf(">", namePos);
-		var nameEnd = innerhtml.indexOf("<", nameStart);
-		var name = innerhtml.substring(nameStart+1, nameEnd);
-		*/
+	//Methoden
+	this.applyAnswerPossibilities = function(){
+		if(this.button1){
+			this.divBottom.removeChild(this.button1);
+			delete this.button1;
+		}
+		if(this.button2){
+			this.divBottom.removeChild(this.button2);
+			delete this.button2;
+		}
+		if(this.button3){
+			this.divBottom.removeChild(this.button3);
+			delete this.button3;
+		}
+		switch(gc.answerNum){
+			case 0:
+				break;
+			case 1:
+				this.button1 = htmlElement('button', "card_"+this.index, "Play");
+				this.divBottom.appendChild(this.button1);
+				break;
+			case 2:
+				this.button1 = htmlElement('button', "card_"+this.index, "Play as 1");
+				this.button2 = htmlElement('button', "card_"+this.index, "Play as 2");
+				this.divBottom.appendChild(this.button1);
+				this.divBottom.appendChild(this.button2);
+				break;
+			case 3:
+				this.button1 = htmlElement('button', "card_"+this.index, "Play 1");
+				this.button2 = htmlElement('button', "card_"+this.index, "Play 2");
+				this.button3 = htmlElement('button', "card_"+this.index, "Play 3");
+				this.divBottom.appendChild(this.button1);
+				this.divBottom.appendChild(this.button2);
+				this.divBottom.appendChild(this.button3);
+				break;
+			default:
+				console.log("setAnserNum("+num+") failed");
+				break;
+		}
 		
-		console.log("pressed button1 on: "+this.text);
-	});
-	this.button2.addEventListener('click', function(){
-		console.log("pressed button2 on: "+this.text);
-	});
+		if(this.button1){
+			this.button1.addEventListener('click', function(){
+				var cardText = gc.getWhiteCard(parseInt(this.id.substring(this.id.indexOf("_")+1))).text;
+				console.log("pressed button1 on: "+cardText);
+			});
+		}
+		if(this.button2){
+			this.button2.addEventListener('click', function(){
+				var cardText = gc.getWhiteCard(parseInt(this.id.substring(this.id.indexOf("_")+1))).text;
+				console.log("pressed button2 on: "+this.text);
+			});
+		}
+		if(this.button3){
+			this.button3.addEventListener('click', function(){
+				var cardText = gc.getWhiteCard(parseInt(this.id.substring(this.id.indexOf("_")+1))).text;
+				console.log("pressed button3 on: "+this.text);
+			});
+		}
+	}
+	
 	this.spanClose.addEventListener('click', function(){
-		console.log("pressed spanClose on: "+this.text);
+		var cardText = gc.getWhiteCard(parseInt(this.id.substring(this.id.indexOf("_")+1))).text;
+		console.log("pressed spanClose on: "+cardText);
 	});
 }
 
@@ -245,57 +296,77 @@ var gameID = window.sessionStorage.getItem('gameID');
 });
 */
 
-//TODO: Wenn Karten mit mehreren Antworten, Parameterübergabe hinzufügen
+function addCard(cardText, container){
+	var newCard = new Card(cardText, gc.whiteCards.length, gc.burnable);
+	newCard.applyAnswerPossibilities();
+	container.appendChild(newCard.html);
+	gc.whiteCards.push(newCard);
+}
 //								string[]
-function addCards_slaveWhiteCards(cards, hasDoubleAnswer){
-	for(x in cards) whiteCardContainer.appendChild((new Card(cards[x], true, hasDoubleAnswer)).html);
+function addCards_slaveWhiteCards(cards){
+	for(x in cards) addCard(cards[x], gc.whiteCardContainer);
 }
 function addCards_chooseWinner(cards){
-	for(x in cards) chooseCardContainer.appendChild((new Card(cards[x], false, false)).html);
+	gc.answerNum = 0;
+	for(x in cards) addCard(cards[x], gc.chooseCardContainer);
+}
+function addCustomCard(customText){ //für blank Cards
+	if(gc.blankCardsAllowed) addCard(customText, gc.whiteCardContainer);
 }
 
 function setMode_slaveWhiteCards(){
-	gc.setVisible("content_blackScore", true);
+	gc.setVisible("blackCardCont", true);
+	gc.setVisible("scoreBoardCont", true);
 	gc.setVisible("content_czarWait", false);
 	gc.setVisible("content_chooseWinner", false);
 	gc.setVisible("content_endscreen", false);
 	gc.setVisible("content_whiteCards", true);
+	gc.setVisible("divide_afterBlackScore", true);
 }
 function setMode_czarWait(){
-	gc.setVisible("content_blackScore", true);
+	gc.setVisible("blackCardCont", true);
+	gc.setVisible("scoreBoardCont", true);
 	gc.setVisible("content_czarWait", true);
 	gc.setVisible("content_chooseWinner", false);
 	gc.setVisible("content_endscreen", false);
 	gc.setVisible("content_whiteCards", false);
+	gc.setVisible("divide_afterBlackScore", true);
 }
 function setMode_chooseWinner() {
-	gc.setVisible("content_blackScore", true);
+	gc.setVisible("blackCardCont", true);
+	gc.setVisible("scoreBoardCont", true);
 	gc.setVisible("content_czarWait", false);
 	gc.setVisible("content_chooseWinner", true);
 	gc.setVisible("content_endscreen", false);
 	gc.setVisible("content_whiteCards", false);
+	gc.setVisible("divide_afterBlackScore", true);
 }
 function setMode_endscreen(){
-	gc.setVisible("content_blackScore", false);
+	gc.setVisible("blackCardCont", false);
+	gc.setVisible("scoreBoardCont", true);
 	gc.setVisible("content_czarWait", false);
 	gc.setVisible("content_chooseWinner", false);
 	gc.setVisible("content_endscreen", true);
 	gc.setVisible("content_whiteCards", false);
+	gc.setVisible("divide_afterBlackScore", false);
 }
 
 window.onload = function(){
 	gc = new GameContainer();
-	sb = new ScoreBoard(10, ["Player1", "Player2", "dritterSpieler", "der schlechteste Spieler ever!!!"]);
+	
+	//Generate test players
+	sb = new ScoreBoard(["Player1", "Player2", "dritterSpieler", "der schlechteste Spieler ever!!!"]);
 	sb.addPlayer("ich bin auch dabei");
-	sb.countRound();
 	for(let i = 0; i<2; i++){ sb.getPlayer("Player2").increaseScore();}
 	for(let i = 0; i<3; i++){ sb.getPlayer("ich bin auch dabei").increaseScore();}
 	sb.getPlayer("dritterSpieler").increaseScore();
 	sb.addPoint("Player1");
 	
+	//init round (white cards legen)
 	setMode_slaveWhiteCards();
+	gc.setBlackCard("Das hier ________ ist eine automatisch generierte schwarze Karte ________ .", 2);
 	addCards_slaveWhiteCards(["Ich bin die erste Karte.",
 	 						"ich passe nicht in die Lücke, aber bin trotzdem hier.",
 	 						"Hallo, das ist die am besten passende Karte",
-	 						"Und zu guter letzt noch eine weitere nutzlose Karte"], true);
+	 						"Und zu guter letzt noch eine weitere nutzlose Karte"]);
 };
